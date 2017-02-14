aa904ca2ca9db747fe68d7f29e5085a872096f41
John Hoedeman <jhoedeman@thinkfishhook.com>
2017-02-14 13:24:30 -0500

    REFACTOR: Slideable protocol and container vc more generic
    Next step is including the map stuff to make that easier to copy-paste into w50

--- SlideOverMap/TableViewController.swift
+++ SlideOverMap/TableViewController.swift
@@ -7,21 +7,20 @@ import UIKit
 class TableViewController: UITableViewController {
     
     var overScrolling: Bool = false
-    
 }
 
 // MARK: Scroll View Delegate
+
 extension TableViewController {
     
     override func scrollViewDidScroll(_ scrollView: UIScrollView)
     {
         overScrolling = scrollView.contentOffset.y <= 0 ? true : false
-        print(overScrolling)
     }
-    
 }
 
 // MARK: Slideable
+
 extension TableViewController: Slideable {
     
     func didSnapToBottom()
