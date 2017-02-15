7e3f62384be7f784cfbae69939cb70e35ef785bf
Evan DeLaney <evan@fishhook.com>
2017-02-15 16:20:27 -0500

    FEATURE: SlideyController framework

---
+++ SlideyController/SlideyController.h
@@ -0,0 +1,19 @@
+//
+//  SlideyController.h
+//  SlideyController
+//
+//  Created by Evan DeLaney on 2/15/17.
+//  Copyright © 2017 Fish Hook LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlideyController.
+FOUNDATION_EXPORT double SlideyControllerVersionNumber;
+
+//! Project version string for SlideyController.
+FOUNDATION_EXPORT const unsigned char SlideyControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlideyController/PublicHeader.h>
+
+
